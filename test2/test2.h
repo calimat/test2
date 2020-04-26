06f45b1644eca103848c0f92dfa6db660ac664bc
Ricardo Herrera MAC <rherrerapetit@gmail.com>
2020-04-25 23:12:43 -0500

    Initial Commit

---
+++ test2/test2.h
@@ -0,0 +1,19 @@
+//
+//  test2.h
+//  test2
+//
+//  Created by Ricardo Herrera Petit on 4/25/20.
+//  Copyright © 2020 Ricardo Herrera Petit. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for test2.
+FOUNDATION_EXPORT double test2VersionNumber;
+
+//! Project version string for test2.
+FOUNDATION_EXPORT const unsigned char test2VersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <test2/PublicHeader.h>
+
+
